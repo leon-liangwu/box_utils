ac35889a1e75c6424f2266b6ee05b081995c9f19
liang.wu <liang.wu@allride.ai>
2019-05-10 11:22:33 +0800

    modify iou_test.py

--- iou_test.py
+++ iou_test.py
@@ -10,9 +10,17 @@ boxes2 = np.array([[50, -50, 100, 900, -8.],
                   [200, 200, 100, 200, 0.]], np.float32)
 
 
+print('Boxes array 1:')
+print(boxes1)
+
+print('Boxes array 2:')
+print(boxes2)
+
+print('Computing rotated iou with gpu...')
 riou_gpu = rbbx_overlaps(boxes1, boxes2, 0)
 print(riou_gpu)
 
+print('Computing rotated iou with cpu...')
 riou_cpu = rbbx_overlaps(boxes1, boxes2)
 print(riou_cpu)
 
